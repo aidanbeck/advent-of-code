f582ddc3d06979c83913f8768674bd707cb2aaca
aidanbeck <aidanmbeck@outlook.com>
2025-12-14 14:43:55 -0600

    Lobby tests

---
+++ src/main/kotlin/com/aidanbeck/advent_of_code/Lobby.kt
@@ -0,0 +1,7 @@
+package com.aidanbeck.advent_of_code
+
+class Lobby {
+    fun getJoltage(batteries: String) = "0"
+    fun getJoltages(banks: Array<String>) = arrayOf("0")
+    fun solvePartOne(puzzleInput: String) = 0
+}
---
+++ src/test/kotlin/com/aidanbeck/advent_of_code/LobbyTest.kt
@@ -0,0 +1,39 @@
+package com.aidanbeck.advent_of_code
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class LobbyTest {
+    @Test
+    fun testGetJoltage() {
+
+        val lobby = Lobby()
+
+        assertEquals("98", lobby.getJoltage("987654321111111"))
+        assertEquals("89", lobby.getJoltage("811111111111119"))
+        assertEquals("78", lobby.getJoltage("234234234234278"))
+        assertEquals("92", lobby.getJoltage("818181911112111"))
+    }
+
+    @Test
+    fun testGetJoltages() {
+        val lobby =  Lobby()
+
+        val banks = arrayOf("987654321111111", "811111111111119", "234234234234278", "818181911112111")
+        val joltages = arrayOf("98", "89", "78", "92")
+
+        assertEquals(joltages, lobby.getJoltages(banks))
+    }
+
+    @Test
+    fun testSolvePartOne() {
+        val lobby = Lobby()
+        val puzzleInput =
+"""987654321111111
+811111111111119
+234234234234278
+818181911112111"""
+
+        assertEquals(357, lobby.solvePartOne(puzzleInput))
+    }
+}
