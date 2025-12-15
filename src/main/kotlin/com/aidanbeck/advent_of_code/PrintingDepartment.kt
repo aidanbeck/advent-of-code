cdcb5e616c19bb5a19bc11687320f9a89f8be41c
aidanbeck <aidanmbeck@outlook.com>
2025-12-15 15:31:22 -0600

    PrintingDepartment get width and height of grid

--- src/main/kotlin/com/aidanbeck/advent_of_code/PrintingDepartment.kt
+++ src/main/kotlin/com/aidanbeck/advent_of_code/PrintingDepartment.kt
@@ -1,4 +1,17 @@
 package com.aidanbeck.advent_of_code
 
-class PrintingDepartment {
+class PrintingDepartment(val tiles: String) {
+    var width = calculateWidth()
+    var height = calculateHeight()
+
+    fun calculateWidth(): Int {
+        return tiles.indexOf('\n')
+    }
+    fun calculateHeight(): Int {
+        return tiles.length / tiles.indexOf('\n')
+    }
+    fun getTile(x: Int, y: Int) = '.'
+    fun getAdjacentTiles(x: Int, y: Int) = "........"
+    fun isTileMoveable(x: Int, y: Int) = false
+    fun countMoveableTiles() = 0
 }
--- src/test/kotlin/com/aidanbeck/advent_of_code/PrintingDepartmentTest.kt
+++ src/test/kotlin/com/aidanbeck/advent_of_code/PrintingDepartmentTest.kt
@@ -1,4 +1,68 @@
 package com.aidanbeck.advent_of_code
 
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlin.test.assertFalse
+
 class PrintingDepartmentTest {
+
+    // global puzzle solution
+    val puzzleInput =
+"""..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@."""
+
+    @Test
+    fun testCalculateWidth() {
+        val printingDepartment = PrintingDepartment(puzzleInput)
+        assertEquals(10, printingDepartment.calculateWidth())
+    }
+
+    @Test
+    fun testCalculatHeight() {
+        val printingDepartment = PrintingDepartment(puzzleInput)
+        assertEquals(10, printingDepartment.calculateHeight())
+    }
+
+    @Test
+    fun testGetTile() {
+        val printingDepartment = PrintingDepartment(puzzleInput)
+        assertEquals('@', printingDepartment.getTile(4,4))
+        assertEquals('@', printingDepartment.getTile(8,9))
+        assertEquals('.', printingDepartment.getTile(6,3))
+        assertEquals('@', printingDepartment.getTile(7,9))
+    }
+
+    @Test
+    fun testGetAdjacentTiles() {
+        // character order should be clockwise, starting at 12:00
+        // return @, ., or _
+        val printingDepartment = PrintingDepartment(puzzleInput)
+        assertEquals("__.@@___", printingDepartment.getAdjacentTiles(0,0))
+        assertEquals("@@@.@@@@", printingDepartment.getAdjacentTiles(4,6))
+        assertEquals("._____@@", printingDepartment.getAdjacentTiles(9,9))
+    }
+
+    @Test
+    fun testIsTileMoveable() {
+        val printingDepartment = PrintingDepartment(puzzleInput)
+        assertTrue(printingDepartment.isTileMoveable(2, 0))
+        assertTrue(printingDepartment.isTileMoveable(6, 2))
+        assertFalse(printingDepartment.isTileMoveable(4, 7))
+        assertFalse(printingDepartment.isTileMoveable(8, 2))
+    }
+
+    @Test
+    fun testCountMoveableTiles() {
+        val printingDepartment = PrintingDepartment(puzzleInput)
+        assertEquals(13, printingDepartment.countMoveableTiles())
+    }
 }
