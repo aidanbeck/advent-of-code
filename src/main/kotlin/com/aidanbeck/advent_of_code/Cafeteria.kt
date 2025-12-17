cc674828603953b422e7b9e1d845aa1b059cf1d2
aidanbeck <aidanmbeck@outlook.com>
2025-12-17 00:03:02 -0600

    parseRanges & parseIds

--- src/main/kotlin/com/aidanbeck/advent_of_code/Cafeteria.kt
+++ src/main/kotlin/com/aidanbeck/advent_of_code/Cafeteria.kt
@@ -1,2 +1,40 @@
 package com.aidanbeck.advent_of_code
 
+// class Range(min: Int, max: Int) // already defined in GiftShop- can I just reuse from there?
+
+class Cafeteria(val puzzleInput: String) {
+
+    val ranges = parseRanges()
+    val ids = parseIds()
+
+    fun parseRanges(): Array<Range> {
+        val rangeStrings = puzzleInput.split("\n\n")[0].split('\n') // get all lines BEFORE empty line
+        val ranges = ArrayList<Range>()
+
+        for (rangeString in rangeStrings) {
+            val minMaxStrings: List<String> = rangeString.split('-')
+            val min = minMaxStrings[0].toLong()
+            val max = minMaxStrings[1].toLong()
+
+            ranges.add( Range(min, max) )
+        }
+
+        return ranges.toTypedArray()
+    }
+
+    fun parseIds(): Array<Int> {
+        val idStrings = puzzleInput.split("\n\n")[1].split('\n') // get all lines AFTER empty line
+        val ids = ArrayList<Int>()
+
+        for (idString in idStrings) {
+            ids.add( idString.toInt() )
+        }
+
+        return ids.toTypedArray()
+    }
+
+    fun isIdWithinRange(id: Int, range: Range) = false
+    fun isIdFresh(id: Int) = false
+    fun getFreshIds() = arrayOf(0)
+    fun countFreshIds() = 0
+}
--- src/test/kotlin/com/aidanbeck/advent_of_code/CafeteriaTest.kt
+++ src/test/kotlin/com/aidanbeck/advent_of_code/CafeteriaTest.kt
@@ -1,4 +1,66 @@
 package com.aidanbeck.advent_of_code
 
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlin.test.assertFalse
+
 class CafeteriaTest {
+    val puzzleInput =
+"""3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32"""
+
+    @Test
+    fun testParseRanges() {
+        val cafeteria = Cafeteria(puzzleInput)
+        val ranges = arrayOf(
+            Range(3,5),
+            Range(10,14),
+            Range(16,20),
+            Range(12,18)
+        )
+        assertContentEquals(ranges, cafeteria.parseRanges())
+    }
+
+    @Test
+    fun testParseIds() {
+        val cafeteria = Cafeteria(puzzleInput)
+        
+        val ids = arrayOf(1, 5, 8, 11, 17, 32)
+        assertContentEquals(ids, cafeteria.parseIds())
+    }
+
+    @Test
+    fun testIsIdWithinRange() {
+        val cafeteria = Cafeteria(puzzleInput)
+        assertTrue(cafeteria.isIdWithinRange(10, Range(5, 15) ) )
+        assertTrue(cafeteria.isIdWithinRange(5, Range(5, 15) ) )
+        assertTrue(cafeteria.isIdWithinRange(15, Range(5, 15) ) )
+
+        assertFalse(cafeteria.isIdWithinRange(16, Range(5, 15) ) )
+    }
+
+    @Test
+    fun testGetFreshIds() {
+        val cafeteria = Cafeteria(puzzleInput)
+        val freshIds = arrayOf(5, 11, 17)
+
+        assertContentEquals(freshIds, cafeteria.getFreshIds())
+    }
+
+    @Test
+    fun testCountFreshIds() {
+        val cafeteria = Cafeteria(puzzleInput)
+        assertEquals(3, cafeteria.countFreshIds())
+    }
 }
